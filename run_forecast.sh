70920735990eada4858ae70c241d9e6e99166e2e
tokyobrandon347@disroot.org <tokyobrandon347@disroot.org>
2024-06-04 11:40:11 -0700

    new

--- run_forecast.sh
+++ run_forecast.sh
@@ -19,8 +19,10 @@ if [ ! -f forecast.wav ]; then
 fi
 
 # Convert forecast.wav to forecast.mp3 using ffmpeg
-#ffmpeg -y -i forecast.wav forecast.mp3
-ffmpeg -y -i start.mp3 -i forecast.wav -filter_complex "[1]adelay=500|500[delayed];[0][delayed]concat=n=2:v=0:a=1[out]" -map "[out]" forecast.mp3
+#ffmpeg -y -i start.mp3 -i forecast.wav -filter_complex "[1]adelay=500|500[delayed];[0][delayed]concat=n=2:v=0:a=1[out]" -map "[out]" forecast.mp3
+#ffmpeg -i forecast.wav -y forecast.mp3
+ffmpeg -i forecast.wav -filter_complex "aevalsrc=0:d=5[silence];[0:a][silence]concat=n=2:v=0:a=1[a]" -map "[a]" -y forecast.mp3
+
 
 
 
@@ -32,7 +34,8 @@ if [ ! -f forecast.mp3 ]; then
 fi
 
 # Play the forecast.mp3 using mpv
-mpv forecast.mp3
+#mpv forecast.mp3
+mpv --loop=inf
 
 # Cleanup temporary files
 rm forecast.wav
